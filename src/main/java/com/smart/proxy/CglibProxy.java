e727f4d8d695b37cf9ca858d495b106ec6f008d4
niuhaijun <niuhaijun@100tal.com>
2018-08-08 14:49:05 +0800

    添加注释

--- src/main/java/com/smart/proxy/CglibProxy.java
+++ src/main/java/com/smart/proxy/CglibProxy.java
@@ -10,13 +10,23 @@ public class CglibProxy implements MethodInterceptor {
   private Enhancer enhancer = new Enhancer();
 
   public Object getProxy(Class clazz) {
-    enhancer.setSuperclass(clazz);
+    enhancer.setSuperclass(clazz);//设置需要被代理的目标类
     enhancer.setCallback(this);
-    return enhancer.create();
+    return enhancer.create();//通过字节码技术动态创建子类实例
   }
 
-  public Object intercept(Object obj, Method method, Object[] args,
-      MethodProxy proxy) throws Throwable {
+  /**
+   * 拦截父类，也就是目标类所有方法的调用。将横切逻辑与业务逻辑编织在一起
+   * @param obj 目标类的对象
+   * @param method 方法类方法的对象
+   * @param args 动态入参
+   * @param proxy 代理类实例
+   * @return 方法返回值
+   * @throws Throwable 异常
+   */
+  @Override
+  public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy)
+      throws Throwable {
     PerformanceMonitor.begin(obj.getClass().getName() + "." + method.getName());
     Object result = proxy.invokeSuper(obj, args);
     PerformanceMonitor.end();
