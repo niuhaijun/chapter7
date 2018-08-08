2cd1157a7fe9eea82f368bbe668eb0d98cc0ee3e
niuhaijun <niuhaijun@100tal.com>
2018-08-08 11:27:07 +0800

    调整代码

--- src/test/java/com/smart/proxy/ForumServiceTest.java
+++ src/test/java/com/smart/proxy/ForumServiceTest.java
@@ -1,32 +1,43 @@
 package com.smart.proxy;
 
+import java.lang.reflect.Proxy;
 import org.testng.annotations.Test;
 
 public class ForumServiceTest {
 
-
+  /**
+   * 业务类正常编码的测试
+   */
   @Test
-  public void proxy() {
-    // 业务类正常编码的测试
-    // ForumService forumService = new ForumServiceImpl();
-    // forumService.removeForum(10);
-    // forumService.removeTopic(1012);
+  public void noneProxy() {
+    ForumService forumService = new ForumServiceImpl();
+    forumService.removeForum(10);
+    forumService.removeTopic(1012);
+  }
 
-    // 使用JDK动态代理
-/*		ForumService target = new ForumServiceImpl();
+  /**
+   * 使用JDK动态代理
+   */
+  @Test
+  public void jdkProxy() {
+    ForumService target = new ForumServiceImpl();
     PerformaceHandler handler = new PerformaceHandler(target);
-		ForumService proxy = (ForumService) Proxy.newProxyInstance(target
-				.getClass().getClassLoader(),
-				target.getClass().getInterfaces(), handler);
-		proxy.removeForum(10);
-		proxy.removeTopic(1012);*/
+    ForumService proxy = (ForumService) Proxy.newProxyInstance(target
+        .getClass().getClassLoader(), target.getClass().getInterfaces(), handler);
+    proxy.removeForum(10);
+    proxy.removeTopic(1012);
+  }
 
-    //使用CGLib动态代理
+  /**
+   * 使用CGLib动态代理
+   */
+  @Test
+  public void cglibProxy() {
     CglibProxy cglibProxy = new CglibProxy();
     ForumService forumService = (ForumService) cglibProxy.getProxy(ForumServiceImpl.class);
     forumService.removeForum(10);
     forumService.removeTopic(1023);
-
   }
+
 
 }
