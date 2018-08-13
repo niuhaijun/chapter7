b20cc075431940ce410db5106b3f5241f32891e1
niuhaijun <niuhaijun@100tal.com>
2018-08-13 09:09:15 +0800

    添加注释

--- src/main/java/com/smart/advice/GreetingAfterAdvice.java
+++ src/main/java/com/smart/advice/GreetingAfterAdvice.java
@@ -15,7 +15,9 @@ public class GreetingAfterAdvice implements AfterReturningAdvice {
    * @param method 目标类的方法
    * @param args 目标实例方法的入参
    * @param obj 目标类实例
-   * @throws Throwable 异常
+   * @throws Throwable 如果在此方法抛出了异常。
+   *  1 抛出的异常是目标方法声明的异常，则该异常归并到目标方法中
+   *  2 抛出的异常不是目标方法声明的异常，则Spring将其转化为运行期异常抛出
    */
   @Override
   public void afterReturning(Object returnObj, Method method, Object[] args,
