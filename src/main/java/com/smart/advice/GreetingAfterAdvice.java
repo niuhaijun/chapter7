427e26812b831b01f8cc6d837ccc557a85d14282
niuhaijun <niuhaijun@100tal.com>
2018-08-13 09:04:35 +0800

    添加注释

--- src/main/java/com/smart/advice/GreetingAfterAdvice.java
+++ src/main/java/com/smart/advice/GreetingAfterAdvice.java
@@ -8,6 +8,15 @@ import org.springframework.aop.AfterReturningAdvice;
  */
 public class GreetingAfterAdvice implements AfterReturningAdvice {
 
+  /**
+   * 横切逻辑
+   *
+   * @param returnObj 目标实例方法返回的结果
+   * @param method 目标类的方法
+   * @param args 目标实例方法的入参
+   * @param obj 目标类实例
+   * @throws Throwable 异常
+   */
   @Override
   public void afterReturning(Object returnObj, Method method, Object[] args,
       Object obj) throws Throwable {
