1bb198efad315a43bb593f35afea750f2353eb13
niuhaijun <niuhaijun@100tal.com>
2018-08-13 10:01:33 +0800

    添加注释

--- src/test/java/com/smart/introduce/IntroduceTest.java
+++ src/test/java/com/smart/introduce/IntroduceTest.java
@@ -7,12 +7,15 @@ import org.testng.annotations.Test;
 public class IntroduceTest {
 
   @Test
-  public void introduce() {
+  public void testIntroductionInterceptor() {
     String configPath = "com/smart/introduce/beans.xml";
     ApplicationContext ctx = new ClassPathXmlApplicationContext(configPath);
     ForumService forumService = (ForumService) ctx.getBean("forumService");
     forumService.removeForum(10);
     forumService.removeTopic(1022);
+
+    System.out.println("===============================");
+
     Monitorable moniterable = (Monitorable) forumService;
     moniterable.setMonitorActive(true);
     forumService.removeForum(10);
