1cd1184d3178f892845c07d57a986286d3f9f1be
niuhaijun <niuhaijun@100tal.com>
2018-08-28 09:36:33 +0800

    注释

--- src/main/java/com/smart/advisor/GreetingAdvisor.java
+++ src/main/java/com/smart/advisor/GreetingAdvisor.java
@@ -4,12 +4,21 @@ import java.lang.reflect.Method;
 import org.springframework.aop.ClassFilter;
 import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
 
+/**
+ * 静态方法匹配器切点切面
+ */
 public class GreetingAdvisor extends StaticMethodMatcherPointcutAdvisor {
 
+  /**
+   * 切点方法匹配规则：只匹配名为greetTo的方法
+   */
   public boolean matches(Method method, Class clazz) {
     return "greetTo".equals(method.getName());
   }
 
+  /**
+   * 切点类匹配规则：只匹配Waiter或者Waiter的子类。默认匹配所有类
+   */
   public ClassFilter getClassFilter() {
     return new ClassFilter() {
       public boolean matches(Class clazz) {
