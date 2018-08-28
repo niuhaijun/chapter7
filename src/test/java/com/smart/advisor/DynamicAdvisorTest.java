532f954e5c486fcec3f2f612f67cedf4906d1e41
niuhaijun <niuhaijun@100tal.com>
2018-08-28 14:54:02 +0800

    格式化

--- src/test/java/com/smart/advisor/DynamicAdvisorTest.java
+++ src/test/java/com/smart/advisor/DynamicAdvisorTest.java
@@ -4,13 +4,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.annotations.Test;
 
+/**
+ * 动态切面
+ */
 public class DynamicAdvisorTest {
 
   @Test
   public void dynamic() {
     String configPath = "com/smart/advisor/beans.xml";
     ApplicationContext ctx = new ClassPathXmlApplicationContext(configPath);
-    Waiter waiter = (Waiter) ctx.getBean("waiter2");
+    Waiter waiter = ctx.getBean("waiter2", Waiter.class);
     waiter.serveTo("Peter");
     waiter.greetTo("Peter");
     waiter.serveTo("Peter");
