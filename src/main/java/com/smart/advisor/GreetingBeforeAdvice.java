e2041b86ceaf0cf7979a3cae0de90f25d85ad8ef
niuhaijun <niuhaijun@100tal.com>
2018-08-28 09:22:48 +0800

    注释

--- src/main/java/com/smart/advisor/GreetingBeforeAdvice.java
+++ src/main/java/com/smart/advisor/GreetingBeforeAdvice.java
@@ -3,8 +3,12 @@ package com.smart.advisor;
 import java.lang.reflect.Method;
 import org.springframework.aop.MethodBeforeAdvice;
 
+/**
+ * 前置增强
+ */
 public class GreetingBeforeAdvice implements MethodBeforeAdvice {
 
+  @Override
   public void before(Method method, Object[] args, Object obj) throws Throwable {
     String clientName = (String) args[0];
     System.out.println(obj.getClass().getName() + "." + method.getName());
