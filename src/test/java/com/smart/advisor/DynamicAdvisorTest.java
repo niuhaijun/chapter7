f669f61d6d5b3581f4c8b28fc672c3f72b1f7528
niuhaijun <niuhaijun@100tal.com>
2018-08-28 15:11:00 +0800

    动态切点运行信息

--- src/test/java/com/smart/advisor/DynamicAdvisorTest.java
+++ src/test/java/com/smart/advisor/DynamicAdvisorTest.java
@@ -20,3 +20,31 @@ public class DynamicAdvisorTest {
     waiter.greetTo("John");
   }
 }
+/*
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.serveTo做静态检查.
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.greetTo做静态检查.
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.setSelfProxy做静态检查.
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.toString做静态检查.
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.clone做静态检查.
+
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.serveTo做静态检查.
+waiter serving Peter...
+
+调用getClassFilter()对com.smart.advisor.Waiter做静态检查.
+调用matches(method,clazz)对com.smart.advisor.Waiter.greetTo做静态检查.
+调用matches(method,clazz,args)对com.smart.advisor.Waiter.greetTo做动态检查.
+waiter greet to Peter...
+
+waiter serving Peter...
+
+调用matches(method,clazz,args)对com.smart.advisor.Waiter.greetTo做动态检查.
+com.smart.advisor.Waiter.greetTo
+How are you！Mr.John.
+waiter greet to John...
+*/
