e562536db86460166f50240f5ee712f3d7da6325
niuhaijun <niuhaijun@100tal.com>
2018-08-28 09:52:18 +0800

    更换类名

--- src/test/java/com/smart/advisor/StaticMethodMatcherPointcutAdvisorTest.java
+++ src/test/java/com/smart/advisor/StaticMethodMatcherPointcutAdvisorTest.java
@@ -4,14 +4,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.annotations.Test;
 
-public class StaticMethodAdvisorTest {
+public class StaticMethodMatcherPointcutAdvisorTest {
 
   @Test
   public void staticMethod() {
     String configPath = "com/smart/advisor/beans.xml";
     ApplicationContext ctx = new ClassPathXmlApplicationContext(configPath);
-    Waiter waiter = (Waiter) ctx.getBean("waiter");
-    Seller seller = (Seller) ctx.getBean("seller");
+    Waiter waiter =  ctx.getBean("waiter", Waiter.class);
+    Seller seller =ctx.getBean("seller", Seller.class);
     waiter.greetTo("John");
     waiter.serveTo("John");
     seller.greetTo("John");
